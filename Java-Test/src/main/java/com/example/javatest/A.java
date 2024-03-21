2cf39629fad3c0aa07aca9ccabee8e7d1803b029
JoJaeHyeon <joje1003@naver.com>
2024-03-21 12:13:18 +0900

    test : syncronized 클래스 단위의 테스트

--- Java-Test/src/main/java/com/example/javatest/A.java
+++ Java-Test/src/main/java/com/example/javatest/A.java
@@ -2,17 +2,17 @@ package com.example.javatest;
 
 public class A {
 
-	public synchronized void run(String name){
+	public static synchronized void run(String name){
 		System.out.println(name + "lock");
 		try{
 			Thread.sleep(1000);
 		} catch (InterruptedException e){
 			e.printStackTrace();
 		}
-		System.out.println(name + "lock");
+		System.out.println(name + "unlock");
 	}
 
-	public void print(final String name) {
-		System.out.println("hi");
+	public static synchronized void print(String name) {
+		System.out.println(name + "hi");
 	}
 }
--- Java-Test/src/main/java/com/example/javatest/JavaTestApplication.java
+++ Java-Test/src/main/java/com/example/javatest/JavaTestApplication.java
@@ -2,15 +2,13 @@ package com.example.javatest;
 
 public class JavaTestApplication {
 
-	public static void main(String[] args) throws InterruptedException{
-		A a = new A();
-		Thread thread1 = new Thread(() -> a.run("thread1"));
-		Thread thread2 = new Thread(() -> a.print("thread2"));
+	public static void main(String[] args) {
+		A a1 = new A();
+		A a2 = new A();
+		Thread thread1 = new Thread(() -> a1.run("thread1"));
+		Thread thread2 = new Thread(() -> a2.run("thread2"));
 		thread1.start();
-		Thread.sleep(500);
 		thread2.start();
-
-		//syncronized 키워드가 없는 경우, 락과 상관 없이 별도로 잘 실행된다.
 	}
 
 }
