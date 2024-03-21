47f429f3c14fc7534632c6fea4705e554f02539a
JoJaeHyeon <joje1003@naver.com>
2024-03-21 12:06:08 +0900

    test : syncronized method 별개 객체 생성 테스트

--- Java-Test/src/main/java/com/example/javatest/JavaTestApplication.java
+++ Java-Test/src/main/java/com/example/javatest/JavaTestApplication.java
@@ -3,11 +3,13 @@ package com.example.javatest;
 public class JavaTestApplication {
 
 	public static void main(String[] args) {
-		A a = new A();
-		Thread thread1 = new Thread(() -> a.run("thread1"));
-		Thread thread2 = new Thread(() -> a.run("thread2"));
+		A a1 = new A();
+		A a2 = new A();
+		Thread thread1 = new Thread(() -> a1.run("thread1"));
+		Thread thread2 = new Thread(() -> a2.run("thread2"));
 		thread1.start();
 		thread2.start();
+		// 서로 다른 객체에  대한 접근이니 lock을 공유하지 않아 동기화가 발생하지 않음
 	}
 
 }
