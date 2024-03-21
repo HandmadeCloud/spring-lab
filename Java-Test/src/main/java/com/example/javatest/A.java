dbbb02d7afe18394f2174aacf0647f185b30d652
JoJaeHyeon <joje1003@naver.com>
2024-03-21 12:04:56 +0900

    test : syncronized method 범위 테스트

---
+++ Java-Test/src/main/java/com/example/javatest/A.java
@@ -0,0 +1,14 @@
+package com.example.javatest;
+
+public class A {
+
+	public synchronized void run(String name){
+		System.out.println(name + "lock");
+		try{
+			Thread.sleep(1000);
+		} catch (InterruptedException e){
+			e.printStackTrace();
+		}
+		System.out.println(name + "lock");
+	}
+}
--- Java-Test/src/main/java/com/example/javatest/JavaTestApplication.java
+++ Java-Test/src/main/java/com/example/javatest/JavaTestApplication.java
@@ -2,24 +2,12 @@ package com.example.javatest;
 
 public class JavaTestApplication {
 
-	public static void main(String[] args) throws InterruptedException {
-		Thread thread = new Thread(new RunnableThread());
-
-		System.out.println("before start: " + Thread.currentThread());
-		thread.start(); // 스레드 실행
-		System.out.println("after start: " + Thread.currentThread());
-
-		System.out.println("before join: " + Thread.currentThread());
-		thread.join(); // thread 작업 끝날 때까지 메인 스레드 일시정지
-		System.out.println("after join: " + Thread.currentThread());
+	public static void main(String[] args) {
+		A a = new A();
+		Thread thread1 = new Thread(() -> a.run("thread1"));
+		Thread thread2 = new Thread(() -> a.run("thread2"));
+		thread1.start();
+		thread2.start();
 	}
 
-
-	private static final class RunnableThread implements Runnable {
-
-		@Override
-		public void run() {
-			System.out.println("run method: " + Thread.currentThread());
-		}
-	}
 }
