0a7ab9df7fc60f5a20269de85c320e94ee90b3a8
JoJaeHyeon <joje1003@naver.com>
2024-08-18 18:17:08 +0900

    test : static 중첩 클래스 테스트

--- spring-lab-jpa/src/main/java/com/example/springlabjpa/SpringLabJpaApplication.java
+++ spring-lab-jpa/src/main/java/com/example/springlabjpa/SpringLabJpaApplication.java
@@ -1,13 +1,52 @@
 package com.example.springlabjpa;
 
+import java.sql.SQLOutput;
+import java.util.LinkedList;
+import java.util.List;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.util.StringUtils;
 
-@SpringBootApplication
+import lombok.Getter;
+
+// @SpringBootApplication
+// public class SpringLabJpaApplication {
+//
+// 	public static void main(String[] args) {
+// 		SpringApplication.run(SpringLabJpaApplication.class, args);
+// 	}
+//
+// }
+
 public class SpringLabJpaApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringLabJpaApplication.class, args);
+		new School.Student();
 	}
+
+}
+
+class School {
+
+	private static String schoolName = "gangmanSchool";
+	private boolean isOpen = true;
+	private int code = 10000;
+
+	public static class Student{
+		private static String name = "cheolsu";
+		public static String nickname = "goodguy";
+
+		Student(){
+			System.out.println("School의 schoolName : " + schoolName);
+			System.out.println("School non-static 변수 : " + new School().isOpen);
+		}
+	}
+
+	School(){
+		System.out.println("Student의 studentName : " + Student.name);
+		System.out.println("Student의 studentNickname : " + Student.nickname);
+	}
+
 
 }
